4363e850e8fcf76a2bbe7ea2b72d3b5c565b46b5
RezaFerdinal <rezaferdinal0@gmail.com>
2025-03-17 05:17:01 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022300135/obj/Debug/net8.0/tpmodul5_103022300135.AssemblyInfo.cs
+++ tpmodul5_103022300135/obj/Debug/net8.0/tpmodul5_103022300135.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_103022300135")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffdb8d6caa7d3d046d378e67204e0074c89b4da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_103022300135")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_103022300135")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
